c1d1b5cdd5ac4ae25ad236c96b5f6ede192f4bc2
Kidanekal <akidanekal@gmail.com>
2023-05-12 01:33:52 +0900

    made clickable

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ stars = document.getElementsByClassName('star');
 for(i=0;i<stars.length;i++){
     stars[i].style.top=(Math.random()*50) +"vh";
     stars[i].style.left=(Math.random()*100) +"vw";
-}
-window.onload=function () {
+} 
+document.querySelector('.sunwrapper').addEventListener('click',function(){
+    // location.reload();
     document.getElementById('song').play();
-}
-setInterval(function () {
-    document.getElementById('song').currentTime=0;
-},40000)
+})
